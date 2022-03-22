480eaa0a80501a0692150881a2840af57f1abefe
hpcdgrie <dennis.grieger@hlrs.de>
2022-03-22 17:04:11 +0100

    Removed comments

--- CMake/vistle.cmake
+++ CMake/vistle.cmake
@@ -1,9 +1,5 @@
 if(ENABLE_VISTLE)
-find_package(Vistle REQUIRED COMPONENTS sensei sensei_vtk boost_mpi
-)
-  #find_package(vistle_sensei REQUIRED)
-  #find_package(vistle_sensei_vtk REQUIRED)
-  #find_package(vistle_boost_mpi REQUIRED)
+  find_package(Vistle REQUIRED COMPONENTS sensei sensei_vtk boost_mpi)
   add_library(sVistle INTERFACE)
   target_link_libraries(sVistle
     INTERFACE
@@ -12,8 +8,7 @@ find_package(Vistle REQUIRED COMPONENTS sensei sensei_vtk boost_mpi
   )
   get_target_property(vistle_boost_mpi_includes Vistle::vistle_boost_mpi INTERFACE_INCLUDE_DIRECTORIES) 
   target_include_directories(sVistle INTERFACE ${vistle_boost_mpi_includes})      
-  #get_target_property(vistle_sensei_includes Vistle::vistle_sensei INTERFACE_INCLUDE_DIRECTORIES)
-  #target_include_directories(sVistle SYSTEM INTERFACE ${vistle_sensei_includes})
+
   install(TARGETS sVistle EXPORT sVistle)
   install(EXPORT sVistle DESTINATION lib/cmake EXPORT_LINK_INTERFACE_LIBRARIES)
 endif()
